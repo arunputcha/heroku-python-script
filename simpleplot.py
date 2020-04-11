0d93ab155b4521f2851b819f1dc4c2728261cc86
arunputcha <arunputcha@berkeley.edu>
2020-04-11 03:56:49 -0400

    initial commit

--- simpleplot.py
+++ simpleplot.py
@@ -9,6 +9,7 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 import plotly.graph_objects as go
+import plotly.express as px
 from dash.dependencies import Input, Output
 
 #read data
