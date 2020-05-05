a6611eedded97bb03064511eb0789b9403d36e70
arunputcha <arunputcha@berkeley.edu>
2020-05-05 18:56:06 -0400

    initial commit

--- tabs/explain.py
+++ tabs/explain.py
@@ -13,9 +13,6 @@ This project is created as an assignment for the Data-x course in UC Berkeley. W
 test set, and experimented with different models before choosing one with the lowest loss. The table below shows the 
 different models that we tried and their corresponding train and test Root Mean Squared Error (RMSE).
 
-As seen, the XGBoost model has the lowest train and test RMSE and therefore is used as the chosen model to forecast,
-active layer thickness in the Samoylov region in Russia.
-
 
 |         Model         | Train RMSE | Test RMSE |
 |-----------------------|------------|-----------|
@@ -25,18 +22,25 @@ active layer thickness in the Samoylov region in Russia.
 |     Random Forest     |   5.651    |   8.998   |
 |        XGBoost        |   2.194    |   7.342   |
 
+As seen, the XGBoost model has the lowest train and test RMSE and therefore is used as the chosen model to forecast,
+active layer thickness in the Samoylov region in Russia.
+
+
 """),
     
 
 
+dcc.Markdown("""
 
-dcc.Markdown("**Image of the XGBoost model performance against the test data,**"),
+
+**Image of the XGBoost model performance against the test data,**"""),
+
 
 html.Div(children= [
 
-html.Img(src='https://raw.githubusercontent.com/yangluanne/datax-Enviroment-B/master/Webapp/xgboost%20model%20against%20test%20data.png?token=ANXZA3YQPFT4GLQE6UAATZ26XLYK2', style = {'width':'50%', 'height':'50%'}),
+html.Img(src='https://raw.githubusercontent.com/yangluanne/datax-Enviroment-B/master/Webapp/xgboost%20model%20against%20test%20data.png?token=ANXZA3YQPFT4GLQE6UAATZ26XLYK2', style = {'width':'100%', 'height':'100%'}),
     dcc.Markdown("*XGBoost performance against test data (Date on the x-axis, ALT in centimeters on the y-axis)*")],
-    style = {'textAlign':'center'})
+    style = {'textAlign':'left'})
 
 ]
 
